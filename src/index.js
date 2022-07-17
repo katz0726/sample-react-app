8d394ce04f3e29bd16561fa021124e7242f5317b
railsdev <railsdev@localhost.localdomain>
2022-07-17 23:48:18 +0900

    * メッセージアプリの実装

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './index.css';
 import reducer from './reducers';
 
-import App from './components/App';
+import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
+
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={ store }>
-      <App />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={ EventsNew } />
+          <Route path="/" component={ EventsIndex } />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
